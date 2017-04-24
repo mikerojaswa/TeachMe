906699a19d9deff7ca33ef76ffae40de872f4c97
Michael Rojas <mikerojaswa@gmail.com>
2017-04-24 13:01:05 -0700

    First commit

---
+++ questions.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+
+    
+    "QUESTIONS_EN_US" : [
+        {
+            "What is one half plus one fourth?": [
+                "2/4",
+                "2/6",
+                "3/4"
+            ]
+        },
+        {
+            "What is one half plus one fourth?": [
+                "3/4",
+                "1/4",
+                "1/2",
+                "5/16",
+                "2/16"
+            ]
+        }
+        /*,
+        {
+            "What is ten fifteenths + one fifteenth?": [
+                "11/15",
+                "13/14",
+                "14/15",
+                "16/17"
+            ]
+        },
+        {
+            "What is one eighth plus two eighths?": [
+                "3/8",
+                "4/8",
+                "2/64",
+                "3/64"
+            ]
+        },
+        {
+            "What is three eighths plus two eighths?": [
+                "5/8",
+                "6/8",
+                "7/8",
+                "9/8"
+            ]
+        },
+        {
+            "What is two fifths plus one fifth?": [
+                "3/5",
+                "4/5",
+                "2/25",
+                "6/10"
+            ]
+        }  */
+    ]
+    
+};
